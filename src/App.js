5e911b377e6eadc3d094df475dc893a24df29cc5
kiranwankhade <kiranwankhade7738@gamil.com>
2023-09-12 01:03:15 +0530

    add dreamjob component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import DreamJob from './Components/DreamJob';
+import DrivenOutComes from './Components/DrivenOutComes';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <DreamJob/>
+      <DrivenOutComes/>
     </div>
   );
 }
