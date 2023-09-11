624a3b2f5dd2f18f0d91c79ba73cf4e65dfae7e4
shaifali <shaifali02000@gmail.com>
2023-09-11 18:34:55 +0530

    [curriculum,courses,masaiverse,partners Done]:)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import OurCourse from './Components/OurCourse';
+import Masaiverse from './Components/Masaiverse';
+import HiringPartners from './Components/HiringPartners';
+import Curriculum from './Components/Curriculum';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <OurCourse/>
+      <Curriculum/>
+      <Masaiverse/>
+      <HiringPartners/>
     </div>
   );
 }
