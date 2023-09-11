b22f12e58b54335d6a4d9e1cda1ea3024784c3b3
YelveTejas <tejas.yelve20@gmail.com>
2023-09-11 18:48:08 +0530

    done carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Impact from './Components/bottomsection/Impact';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Impact/>
     </div>
   );
 }
